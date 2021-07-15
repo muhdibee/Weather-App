ae8b58cb4c177fdbb7e844ddccba8e0ee2c440ef
Muhammad <muhdibee@gmail.com>
2021-07-15 17:24:18 +0100

    More styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var button = document.querySelector('.button');
-var city = document.querySelector('.city');
+var cityInput = document.querySelector('.city-input-feild');
 var resultcity = document.querySelector('.resultcity');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
@@ -11,27 +11,33 @@ button.addEventListener('click', fetchweather);
 function fetchweather() {
     event.preventDefault();
     var api_key="c1ff2cb016ee3cc0be75ea84f34c24dc";
-    var api = 'https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid='+api_key+'&units=metric';
-    fetch(api)
+    var api = 'https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid='+api_key+'&units=metric';
+    fetch(api, () => {
+        return resultcity.innerHTML = "Loading...";
+    })
     .then(response => response.json())
     .then(data => {
         console.log(data);
         var tempValue = data.main.temp;
+        var country = data.sys.country;
         var descValue = data.weather[0].description;
         var resultcityValue = data.name;
         var celcius = tempValue;
 
-        resultcity.innerHTML = `Weather at ${resultcityValue} is:`;
+        resultcity.innerHTML = `Weather at ${resultcityValue}, ${country}. is:`;
         desc.innerHTML = `${descValue}`;
         temp.innerHTML = `temprature: ${celcius}℃`;
+        cityInput.value = "";
 
     })
-    .catch(err =>{ 
+    .catch(err => { 
        // alert(`${city.value} is a Wrong city name`);
-        resultcity.innerHTML = `${city.value} is not a city please try again.`;
+        resultcity.innerHTML = `City not found`;
         desc.innerHTML = null;
         temp.innerHTML = null;
+        cityInput.value = "";
 
     }) ; 
+
 }
 
